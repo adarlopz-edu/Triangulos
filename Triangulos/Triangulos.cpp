17014d8b49c84cae8356f9670de63315cf4761cd
adarl <adarl@ADARLOPZ-DEV>
2022-10-31 22:12:34 -0600

    Add project files.

---
+++ Triangulos/Triangulos.cpp
@@ -0,0 +1,38 @@
+// Triangulos.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l1, l2, l3;
+    cout << "Ingresa el primer lado del triangulo:";
+    cin >> l1;
+    cout << "Ingresa el segundo lado del triangulo:";
+    cin >> l2;
+    cout << "Ingresa el tercero lado del triangulo:";
+    cin >> l3;
+
+    if(l1==l2 && l1==l3){
+        cout << "El triangulo es equilatero";
+    } else {
+        cout << "El triangulo es un escaleno";
+    }
+    if (l1 == l2 || l2 == l3) {
+        cout << "El triangulo es isoceles";
+    }
+
+
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
